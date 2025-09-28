48b5f73960366feb98dc63be0b7ed8b299124c31
junior8319 <tonhaun@yahoo.com.br>
2025-09-28 18:36:06 -0300

    feat-models layer-set the model "Game Tag".

---
+++ app/models/game.tag.model.ts
@@ -0,0 +1,63 @@
+import { FieldPacket, ResultSetHeader, RowDataPacket } from "mysql2";
+import connection from "./connection";
+import IGameTag from "../interfaces/IGameTag";
+
+const findAll = async ():Promise<IGameTag[]> => {
+  const [rows]: [RowDataPacket[], FieldPacket[]] = await connection.query(
+    `
+      SELECT
+        gt.game_id AS game_id, gt.tag_titlte AS tag_title,
+      FROM games_tags AS gt;
+    `,
+  );
+
+  return rows as IGameTag[];
+};
+
+const createNewGameTag = async (gameTag: IGameTag): Promise<IGameTag | null> => {
+  try {
+    const { gameId, tagId } = gameTag;
+
+    const [result]: [ResultSetHeader, FieldPacket[]] = await connection.query(
+      `
+        INSERT INTO games_tags (game_id, tag_id)
+        VALUES (?, ?);
+      `,
+      [gameId, tagId]
+    );
+
+    if (!result) return null;
+
+    return {
+      ...gameTag,
+    };
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+};
+
+const deleteGameTag = async (gameTagToDelete: IGameTag): Promise<ResultSetHeader | null> => {
+try {
+    const { gameId, tagId } = gameTagToDelete;  
+
+    const [result]: [ResultSetHeader, FieldPacket[]] = await connection.query(
+      `
+        DELETE FROM games_tags
+        WHERE game_id = ?, tag_id = ?;
+      `,
+      [gameId, tagId]
+    );
+
+    if (!result) return null;
+
+    return result;
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+};
+
+export {
+  findAll,
+  createNewGameTag,
+  deleteGameTag,
+};
