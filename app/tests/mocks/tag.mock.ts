8d4b8db2d4024d26d34e7efc4c4b5c8f43f4307f
junior8319 <tonhaun@yahoo.com.br>
2025-10-23 06:11:43 -0300

    chore-mocks-Game Tag: add some mocked Tag objects to a list.

---
+++ app/tests/mocks/tag.mock.ts
@@ -0,0 +1,21 @@
+import ITag from "../../interfaces/ITag";
+
+export const mockTagsList = [
+  {
+    id: 1,
+    title: 'Categoria TST 1',
+  },
+  {
+    id: 2,
+    title: 'Categoria TST 2',
+  },
+  {
+    id: 3,
+    title: 'Categoria TST 3',
+  },
+] as ITag[];
+
+export const mockTag = {
+  id: 1,
+  title: 'Categoria TST 1',
+} as ITag;
