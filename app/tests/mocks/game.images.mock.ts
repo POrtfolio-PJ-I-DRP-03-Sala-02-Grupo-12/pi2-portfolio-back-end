8ae84eacabbe5475d15a1d41012735e2af31810f
junior8319 <tonhaun@yahoo.com.br>
2025-10-23 06:06:52 -0300

    chore-mocks-Game Image: add attributes of relationships and more Game Image objects.

--- app/tests/mocks/game.images.mock.ts
+++ app/tests/mocks/game.images.mock.ts
@@ -17,6 +17,30 @@ export const mockNewGameImage = {
   gameId: 1,
 } as IGameImage;
 
+export const mockGameImage2 = {
+  id: 2,
+  title: 'Imagem para Teste 2',
+  description: 'Descrição da imagem 2',
+  url: 'https://example.com/imagem2.jpg',
+  game: {
+    id: 1,
+    title: 'Jogo para Teste 1',
+    tags: ['Categoria TST 1', 'Categoria TST 2', 'Categoria TST 3']
+  },
+} as IGameImage;
+
+export const mockGameImage3 = {
+  id: 3,
+  title: 'Imagem para Teste 3',
+  description: 'Descrição da imagem 3',
+  url: 'https://example.com/imagem3.jpg',
+  game: {
+    id: 1,
+    title: 'Jogo para Teste 1',
+    tags: ['Categoria TST 1', 'Categoria TST 2', 'Categoria TST 3']
+  },
+} as IGameImage;
+
 export const mockGameImageToUpdate = {
   title: 'Imagem para Teste 1 Alterada',
   url: 'https://example.com/imagem1-alterada.jpg'
@@ -27,13 +51,25 @@ export const mockGameImageUpdated = {
   title: 'Imagem para Teste 1 Alterada',
   description: 'Descrição da imagem 1',
   url: 'https://example.com/imagem1-alterada.jpg',
-  gameId: 1,
+  game: {
+    id: 1,
+    title: 'Jogo para Teste 1',
+    tags: ['Categoria TST 1', 'Categoria TST 2', 'Categoria TST 3']
+  }
 } as IGameImage;
 
-export const mockGameImagesList = [{
-  id: 1,
-  title: 'Imagem para Teste 1 Alterada',
-  description: 'Descrição da imagem 1',
-  url: 'https://example.com/imagem1-alterada.jpg',
-  gameId: 1,
-}] as IGameImage[];
+export const mockGameImagesList = [
+  {
+    id: 1,
+    title: 'Imagem para Teste 1 Alterada',
+    description: 'Descrição da imagem 1',
+    url: 'https://example.com/imagem1-alterada.jpg',
+    game: {
+      id: 1,
+      title: 'Jogo para Teste 1',
+      tags: ['Categoria TST 1', 'Categoria TST 2', 'Categoria TST 3']
+    }
+  },
+  mockGameImage2,
+  mockGameImage3,
+] as IGameImage[];
