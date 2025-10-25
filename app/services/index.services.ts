81611377499e23a90a81798e90c26cf01020e5e5
junior8319 <tonhaun@yahoo.com.br>
2025-10-25 12:11:31 -0300

    chore-backend-services-index: set index file for services layer.

---
+++ app/services/index.services.ts
@@ -0,0 +1,11 @@
+import * as gamesService from './games.service';
+import * as gameImagesService from './game.images.service';
+import * as tagsService from './tags.service';
+import * as gamesTagsService from './games.tags.service';
+
+export {
+  gamesService,
+  gameImagesService,
+  tagsService,
+  gamesTagsService,
+};
