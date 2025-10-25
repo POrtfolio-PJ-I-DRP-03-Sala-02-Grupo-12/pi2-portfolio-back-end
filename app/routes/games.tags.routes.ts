f1c84548ac48631806a6e190fbd0253aa1215b21
junior8319 <tonhaun@yahoo.com.br>
2025-10-25 20:05:19 -0300

    feat-backend-routes-Games Tags: set routes to games tags.

---
+++ app/routes/games.tags.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { gamesTagsController } from "../controllers/index.controllers";
+
+const gamesTagsRouter = Router();
+
+gamesTagsRouter.get("/api/projects/games/tags", gamesTagsController.findAllGamesTags);
+gamesTagsRouter.post("/api/projects/games/tags", gamesTagsController.createNewGameTag);
+gamesTagsRouter.delete("/api/projects/games/tags", gamesTagsController.deleteGameTag);
+
+export default gamesTagsRouter;
