fe030b62c1e920340d40148cf007b51ac620a7d0
junior8319 <tonhaun@yahoo.com.br>
2025-10-25 20:05:48 -0300

    feat-backend-routes-app: use routes in the app.

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express, { NextFunction, Request, Response } from 'express';
 import connection from './models/connection';
+import {
+    gameImagesRouter,
+    gamesRouter,
+    gamesTagsRouter,
+    tagsRouter,
+} from './routes/index.routes';
 
 const app = express();
 
@@ -11,5 +17,10 @@ app.use((req: Request, _res: Response, next: NextFunction) => {
     req.db = connection;
     next();
 });
+
+app.use(gamesRouter);
+app.use(gameImagesRouter);
+app.use(tagsRouter);
+app.use(gamesTagsRouter);
 
 export default app;
