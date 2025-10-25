4874b42da2268567f93f2d3d807f357e558fe8df
junior8319 <tonhaun@yahoo.com.br>
2025-10-25 20:05:33 -0300

    feat-backend-routes-index: set index of routes.

---
+++ app/routes/index.routes.ts
@@ -0,0 +1,11 @@
+import gamesRouter from "./games.routes";
+import gameImagesRouter from "./game.images.routes";
+import tagsRouter from "./tags.routes";
+import gamesTagsRouter from "./games.tags.routes";
+
+export {
+  gamesRouter,
+  gameImagesRouter,
+  tagsRouter,
+  gamesTagsRouter,
+};
