3b24d858a396ba5317baffde7280a1bea474b711
junior8319 <tonhaun@yahoo.com.br>
2025-10-27 13:06:58 -0300

    fix-routes-Tags: fixes the url and the request parameter on finTagById function call; Fixes the order of the routes.

--- app/routes/tags.routes.ts
+++ app/routes/tags.routes.ts
@@ -3,9 +3,9 @@ import { tagsController } from "../controllers/index.controllers";
 
 const tagsRouter = Router();
 
-tagsRouter.get('/api/projects/tags/', tagsController.findAllTags);
-tagsRouter.get('/api/projects/tags/id/:id', tagsController.findTagById);
 tagsRouter.get('/api/projects/tags/search', tagsController.findTagByTitle);
+tagsRouter.get('/api/projects/tags/', tagsController.findAllTags);
+tagsRouter.get('/api/projects/tags/:id', tagsController.findTagById);
 tagsRouter.post('/api/projects/tags/', tagsController.createNewTag);
 tagsRouter.put('/api/projects/tags/:id', tagsController.updateTag);
 tagsRouter.delete('/api/projects/tags/:id', tagsController.deleteTag);
