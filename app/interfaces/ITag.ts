e91fbc2c40cdda93270a11917e4747db845c8d24
junior8319 <tonhaun@yahoo.com.br>
2025-10-27 11:55:57 -0300

    chore-interfaces: sets update result interfaces for Game Images (file IGameImage) and Tags (file ITag).

--- app/interfaces/ITag.ts
+++ app/interfaces/ITag.ts
@@ -1,4 +1,11 @@
+import { ResultSetHeader } from "mysql2/promise";
+
 export default interface ITag {
   id?: number;
   title: string;
+}
+
+export interface ITagUpdateResult {
+  updateResult:ResultSetHeader;
+  updatedTag: ITag;
 }
