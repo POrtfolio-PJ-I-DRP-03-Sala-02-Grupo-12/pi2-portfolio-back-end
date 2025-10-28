8fe7c376766bf0c2bfc6d6b9b0a13e59fa01dfb3
junior8319 <tonhaun@yahoo.com.br>
2025-10-28 18:52:40 -0300

    chore-db-connection: sets "uri" attribute to connect with railway database.

--- app/models/connection.ts
+++ app/models/connection.ts
@@ -2,11 +2,18 @@ import 'dotenv';
 import mysql from 'mysql2/promise'
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+connection.getConnection()
+  .then(() => {
+    console.log('Conectado ao banco de dados no Railway');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar com o banco:', error.message);
+  });
 
 export default connection;
