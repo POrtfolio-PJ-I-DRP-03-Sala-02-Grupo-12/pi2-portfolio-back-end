8b904feeb8de72d1082cbd2d43aab7976d14e1b5
junior8319 <tonhaun@yahoo.com.br>
2025-11-08 09:46:43 -0300

    chore-tests-setup: changes jest.config.js to ES2016.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
-module.exports = {
-  verbose: true,
-  silent: false,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testMatch: [
-    '**/tests/**/*.test.ts',
-    '**/__tests__/**/*.test.ts'
-  ],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  collectCoverageFrom: [
-    'app/**/*.ts',
-    '!app/**/*.d.ts',
-  ],
+export const verbose = true;
+export const silent = false;
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const transform = {
+  '^.+\\.tsx?$': 'ts-jest',
 };
+export const testMatch = [
+  '**/tests/**/*.test.ts',
+  '**/__tests__/**/*.test.ts'
+];
+export const moduleFileExtensions = ['ts', 'js', 'json'];
+export const collectCoverageFrom = [
+  'app/**/*.ts',
+  '!app/**/*.d.ts',
+];
