b1938b826b304f716366afa7b2e8661815d6e17b
junior8319 <tonhaun@yahoo.com.br>
2025-11-09 11:31:27 -0300

    feat-test-services-Games: tests the function findAllGames for a bad request.

--- app/tests/services/games.service.test.ts
+++ app/tests/services/games.service.test.ts
@@ -1,6 +1,17 @@
-import { gameModel } from "../../models/index.model";
-import { findAllGames } from "../../services/games.service";
-import { mockGamesList } from "../mocks/games.mock";
+import {
+  gameModel,
+} from "../../models/index.model";
+import {
+  findAllGames,
+} from "../../services/games.service";
+import {
+  errorMessage,
+  mockError,
+  mockGame1ToInsert,
+  mockGame2ToInsert,
+  mockGame3ToInsert,
+  mockGamesList
+} from "../mocks/games.mock";
 
 jest.mock('../../models/index.model', () => ({
   gameModel: {
@@ -35,6 +46,28 @@ describe('TESTES DO SERVIÇO GAMES', ()=> {
         
         expect(gameModel.findAllGames).toHaveBeenCalledTimes(1);
         expect(result).toHaveLength(3);
+      });
+
+      it('E a lista deve conter os objetos corretos.', async () => {
+        const result = await findAllGames();
+
+        expect(result[0]).toEqual({ id: 1, ...mockGame1ToInsert });
+        expect(result[1]).toEqual({ id: 2, ...mockGame2ToInsert });
+        expect(result[2]).toEqual({ id: 3, ...mockGame3ToInsert });
+      });
+    });
+
+    describe('Em caso de problemas na requisição:', () => {
+      it('Deve retornar uma mensagem de erro', async () => {
+        (gameModel.findAllGames as jest.Mock)
+          .mockClear()
+          .mockRejectedValue(mockError);
+        
+        const result = await findAllGames();
+
+        expect(gameModel.findAllGames).toHaveBeenCalledTimes(1);
+        expect(result).toContain('Erro ao buscar jogos:');
+        expect(result).toContain(errorMessage);
       });
     });
   });
