da712b91f1370d6d41ad4c9e4e4cfd6a54e9500d
junior8319 <tonhaun@yahoo.com.br>
2025-11-17 15:32:03 -0300

    feat-test-services-Game Images: set tests for the function findAllGameImageById.

--- app/tests/services/game.images.service.test.ts
+++ app/tests/services/game.images.service.test.ts
@@ -12,9 +12,19 @@ import {
 } from "../../models/index.model";  
 import {
   findAllGameImages,
+  findGameImageById,
 } from "../../services/game.images.service";  
-import { mockGameImage2, mockGameImage3, mockGameImagesList } from "../mocks/game.images.mock";
-import { errorMessage, mockError } from "../mocks/games.mock";
+import {
+  mockGameImage2,
+  mockGameImage3,
+  mockGameImagesList
+} from "../mocks/game.images.mock";
+import {
+  errorMessage,
+  invalidIdErrorMessage,
+  mockError,
+  mockInvalidIdError
+} from "../mocks/games.mock";
 
 describe('TESTES DO SERVIÇO GAME IMAGES', () => {
   beforeEach(() => {
@@ -67,6 +77,59 @@ describe('TESTES DO SERVIÇO GAME IMAGES', () => {
         expect(gameImageModel.findAllImages).toHaveBeenCalledTimes(1);
         expect(result).toContain('Erro ao buscar imagens de jogos:');
         expect(result).toContain(errorMessage);
+      });
+    });
+  });
+
+  describe('BUSCAR UMA IMAGEM POR ID', () => {
+    describe('Informando um ID que não existe', () => {
+      const nonExistentId = 0;
+      const messageToBeReturned =
+        `Não conseguimos encontrar a imagem do jogo pelo id ${nonExistentId}`;
+      it('Deve retornar uma mensagem de "Não encontrado"', async () => {
+        (gameImageModel.findImageById as jest.Mock)
+          .mockResolvedValue(null);
+        
+        const result = await findGameImageById(0);
+
+        expect(gameImageModel.findImageById).toHaveBeenCalledTimes(1);
+        expect(gameImageModel.findImageById)
+          .toHaveBeenCalledWith(nonExistentId);
+        expect(result).toEqual(messageToBeReturned);
+      });
+    });
+
+    describe('Informando um ID existente', () => {
+      const correctId = 2;
+
+      it('Deve retornar o objeto corretamente.', async () => {
+        (gameImageModel.findImageById as jest.Mock)
+          .mockResolvedValue(mockGameImage2);
+
+        const result = await findGameImageById(correctId);
+
+        expect(gameImageModel.findImageById).toHaveBeenCalledTimes(1);
+        expect(gameImageModel.findImageById).toHaveBeenCalledWith(correctId);
+        expect(result).not.toBeNull();
+        expect(result).toHaveProperty('title');
+        if (typeof result !== 'string') {
+          expect(result.title).toBeDefined();
+          expect(result.title).toEqual(mockGameImage2.title);
+        }
+        expect(result).toEqual(mockGameImage2);
+      });
+    });
+
+    describe('Em caso de problemas na requisição:', () => {
+      it('Deve retornar uma mensagem de erro', async () => {
+        (gameImageModel.findImageById as jest.Mock)
+          .mockRejectedValue(mockInvalidIdError);
+        
+        const result = await findGameImageById(Number('AB001'));
+
+        expect(gameImageModel.findImageById).toHaveBeenCalledTimes(1);
+        expect(result).toContain('Ocorreu um erro na busca:');
+        expect(result).toContain(invalidIdErrorMessage);
       });
     });
   });
