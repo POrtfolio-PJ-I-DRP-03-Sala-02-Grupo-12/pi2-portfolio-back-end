7ac508964c9d54fab97ba3e00cb94c41162c221a
junior8319 <tonhaun@yahoo.com.br>
2025-11-20 16:21:56 -0300

    chore-test-mocks-Game Images: add some mocks to test Tags services.

--- app/tests/mocks/tag.mock.ts
+++ app/tests/mocks/tag.mock.ts
@@ -1,25 +1,40 @@
 import { ResultSetHeader } from "mysql2/promise";
 import ITag from "../../interfaces/ITag";
 
-export const mockTagsList = [
-  {
-    id: 1,
-    title: 'Categoria TST 1',
-  },
-  {
-    id: 2,
-    title: 'Categoria TST 2',
-  },
-  {
-    id: 3,
-    title: 'Categoria TST 3',
-  },
-] as ITag[];
-
-export const mockTag = {
+export const mockTag1 = {
   id: 1,
   title: 'Categoria TST 1',
+};
+
+export const mockTag2 = {
+  id: 2,
+  title: 'Categoria TST 2',
+};
+
+export const mockTag3 = {
+  id: 3,
+  title: 'Categoria TST 3',
+};
+
+export const mockTagsList = [
+  mockTag1,
+  mockTag2,
+  mockTag3,
+] as ITag[];
+
+export const mockNewTag = {
+  title: 'Categoria TST 1',
 } as ITag;
+
+export const mockTagToUpdate = {
+  id: 1,
+  title: 'Categoria TST Atualizada',
+} as ITag;
+
+export const mockUpdatedTag: ITag = {
+  id: 1,
+  title: 'Categoria TST Atualizada',
+};
 
 export const mockResultSetHeader = {
   fieldCount: 0,
@@ -30,3 +45,18 @@ export const mockResultSetHeader = {
   warningStatus: 0,
   changedRows: 0
 } as ResultSetHeader;
+
+export const errorMessage = 'Table gabisou_db_test.tags doesn\'t exist';
+export const mockError = new Error(errorMessage);
+
+export const invalidTitleErrorMessage = 'Unknown column \'NaN\' in \'where\' clause';
+export const mockInvalidTitleError = new Error(invalidTitleErrorMessage);
+
+export const mockUpdateErrorMessage =
+  'Unknown column \'tiitle\' in \'field list\'';
+
+export const mockUpdateError = new Error(mockUpdateErrorMessage);
+
+export const mockTagWithInvalidColumnName = {
+  tiitle: "Tentativa de alterar categoria com nome inválido de campo",
+} as unknown as ITag;
