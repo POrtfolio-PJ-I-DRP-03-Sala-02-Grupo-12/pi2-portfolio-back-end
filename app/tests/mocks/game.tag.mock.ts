9a36b2afd8a83e42e96b4eecec7c3f78882aa70a
junior8319 <tonhaun@yahoo.com.br>
2025-11-20 16:46:03 -0300

    chore-test-mocks-Games Tags: add mock to error and error message.

--- app/tests/mocks/game.tag.mock.ts
+++ app/tests/mocks/game.tag.mock.ts
@@ -26,4 +26,7 @@ export const mockGameTagsList = [
     gameId: 1,
     tagId: 3,
   },
-]
+];
+
+export const errorMessage = 'Table gabisou_db_test.games_tags doesn\'t exist';
+export const mockError = new Error(errorMessage);
