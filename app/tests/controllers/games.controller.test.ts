96bd16bff28d64b55649ec593b69b4ec95688619
junior8319 <tonhaun@yahoo.com.br>
2025-11-25 18:01:13 -0300

    feat-unit-tests-controllers-Games: set test for the function findAllGames (not found).

--- app/tests/controllers/games.controller.test.ts
+++ app/tests/controllers/games.controller.test.ts
@@ -1,4 +1,4 @@
-jest.mock('../services/index.services', () => ({
+jest.mock('../../services/index.services', () => ({
   gamesService: {
     findAllGames: jest.fn(),
     findGameById: jest.fn(),
@@ -7,3 +7,52 @@ jest.mock('../services/index.services', () => ({
     deleteGame: jest.fn(),
   },
 }));
+
+import {
+  Request,
+  Response,
+} from "express";
+import {
+  gamesService,
+} from "../../services/index.services";
+import {
+  findAllGames,
+} from "../../controllers/games.controller";
+
+let mockRequest: Partial<Request>;
+let mockResponse: Partial<Response>;
+
+
+describe('TESTES DO CONTROLLER GAMES', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('LISTAR JOGOS', () => {
+    describe('Caso não haja jogos cadastrados', () => {
+      mockRequest = {};
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      it('Deve retornar status 404', async () => {
+        (gamesService.findAllGames as jest.Mock)
+          .mockResolvedValue('Não encontramos jogos cadastrados.');
+        
+        await findAllGames(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+
+      it('Deve retornar a mensagem correta', async () => {
+        (gamesService.findAllGames as jest.Mock)
+          .mockResolvedValue('Não encontramos jogos cadastrados.');
+        
+        await findAllGames(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith(
+          { message: 'Não encontramos jogos cadastrados.' }
+        );
+      });
+    });
+  });
+});
