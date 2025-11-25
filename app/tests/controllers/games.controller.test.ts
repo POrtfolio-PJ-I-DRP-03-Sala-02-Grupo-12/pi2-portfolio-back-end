484976f151623b76c04e3820edbd1d79711d0f3c
junior8319 <tonhaun@yahoo.com.br>
2025-11-25 18:20:30 -0300

    feat-unit-tests-controllers-Games: set tests for the function findGameById.

--- app/tests/controllers/games.controller.test.ts
+++ app/tests/controllers/games.controller.test.ts
@@ -17,6 +17,7 @@ import {
 } from "../../services/index.services";
 import {
   findAllGames,
+  findGameById,
 } from "../../controllers/games.controller";
 import { mockError, mockGamesList } from "../mocks/games.mock";
 
@@ -102,6 +103,92 @@ describe('TESTES DO CONTROLLER GAMES', () => {
         await findAllGames(mockRequest as Request, mockResponse as Response);
         expect(mockResponse.json).toHaveBeenCalledWith(
           { message: `Erro no servidor ao listar jogos: ${mockError.message}` }
+        );
+      });
+    });
+  });
+
+  describe('BUSCAR JOGO PELO ID', () => {
+    describe('Caso o jogo não seja encontrado', () => {
+      mockRequest = {
+        params: { id: '999' },
+      };
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      
+      it('Deve retornar status 404', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue('Não conseguimos encontrar o jogo pelo id 999');
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+
+      it('Deve retornar a mensagem correta', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue('Não conseguimos encontrar o jogo pelo id 999');
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith(
+          {
+            message: 'Não conseguimos encontrar o jogo pelo id 999',
+          }
+        );
+      });
+    });
+
+    describe('Caso o jogo seja encontrado', () => {
+      mockRequest = {
+        params: { id: '1' },
+      };
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      it('Deve retornar status 200', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue(mockGamesList[0]);
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+      });
+
+      it('Deve retornar o jogo correto', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue(mockGamesList[0]);
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith(mockGamesList[0]);
+      });
+    });
+
+    describe('Caso ocorra um erro', () => {
+      mockRequest = {
+        params: { id: '1' },
+      };
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      it('Deve retornar status 500', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockRejectedValue(mockError);
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.status).toHaveBeenCalledWith(500);
+      });
+
+      it('Deve retornar a mensagem de erro correta', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockRejectedValue(mockError);
+
+        await findGameById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith(
+          { message: `Erro no servidor ao buscar jogo: ${mockError.message}` }
         );
       });
     });
