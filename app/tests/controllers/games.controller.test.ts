262a353950707e744b4fe6c8086133d7bf55829f
junior8319 <tonhaun@yahoo.com.br>
2025-11-25 18:08:50 -0300

    feat-unit-tests-controllers-Games: set test for the function findAllGames (games found).

--- app/tests/controllers/games.controller.test.ts
+++ app/tests/controllers/games.controller.test.ts
@@ -18,6 +18,7 @@ import {
 import {
   findAllGames,
 } from "../../controllers/games.controller";
+import { mockGamesList } from "../mocks/games.mock";
 
 let mockRequest: Partial<Request>;
 let mockResponse: Partial<Response>;
@@ -52,6 +53,30 @@ describe('TESTES DO CONTROLLER GAMES', () => {
         expect(mockResponse.json).toHaveBeenCalledWith(
           { message: 'Não encontramos jogos cadastrados.' }
         );
+      });
+    });
+
+    describe('Caso haja jogos cadastrados', () => {
+      mockRequest = {};
+      mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      it('Deve retornar status 200', async () => {
+        (gamesService.findAllGames as jest.Mock)
+          .mockResolvedValue(mockGamesList);
+
+        await findAllGames(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+      });
+
+      it('Deve retornar a lista de jogos', async () => {
+        (gamesService.findAllGames as jest.Mock)
+          .mockResolvedValue(mockGamesList);
+
+        await findAllGames(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith(mockGamesList);
       });
     });
   });
