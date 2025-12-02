b0aa4fb6e348bc839001bada30074940b4abfbf5
junior8319 <tonhaun@yahoo.com.br>
2025-12-02 19:30:10 -0300

    feat-unit-tests-controllers-Games Tags: findAllGamesTags - tests for can't be found.

---
+++ app/tests/controllers/games.tags.controller.test.ts
@@ -0,0 +1,45 @@
+jest.mock('../../services/index.services'), () => ({
+  gamesTagsService: {
+    findAllGamesTags: jest.fn(),
+    createNewGameTag: jest.fn(),
+    deleteGameTag: jest.fn(),
+  }
+});
+
+import { findAllGamesTags } from "../../controllers/games.tags.controller";
+import { gamesTagsService } from "../../services/index.services";
+import { Request, Response } from "express";
+
+describe('TESTES DO CONTROLLER GAMES TAGS', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('LISTAR ASSOCIAÇÕES DE JOGOS E CATEGORIAS', () => {
+    describe('Caso não haja associações cadastradas', () => {
+      const mockRequest: Partial<Request> = {};
+      const mockResponse: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      const notFoundMessage = 'Não encontramos jogos com categorias associadas.';
+
+      it('Deve retornar status 404', async () => {
+        (gamesTagsService.findAllGamesTags as jest.Mock)
+          .mockResolvedValue(notFoundMessage);
+        
+          await findAllGamesTags(mockRequest as Request, mockResponse as Response);
+          expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+
+      it('Deve retornar a mensagem de não encontrado', async () => {
+        (gamesTagsService.findAllGamesTags as jest.Mock)
+          .mockResolvedValue(notFoundMessage);
+        
+        await findAllGamesTags(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toHaveBeenCalledWith({ message: notFoundMessage });
+      });
+    });
+  });
+});
