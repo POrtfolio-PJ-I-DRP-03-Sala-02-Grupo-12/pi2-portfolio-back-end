0b676a63914be73b1146959a5ee7a4522c27f1b3
junior8319 <tonhaun@yahoo.com.br>
2025-12-12 11:53:58 -0300

    feat-unit-tests-controllers-Games Tags: tests for deleteGameTag function.

--- app/tests/controllers/games.tags.controller.test.ts
+++ app/tests/controllers/games.tags.controller.test.ts
@@ -1,19 +1,37 @@
-jest.mock('../../services/index.services'), () => ({
+jest.mock('../../services/index.services', () => ({
   gamesTagsService: {
     findAllGamesTags: jest.fn(),
     createNewGameTag: jest.fn(),
     deleteGameTag: jest.fn(),
-  }
-});
+  },
 
-import { createNewGameTag, findAllGamesTags } from "../../controllers/games.tags.controller";
-import { gamesTagsService } from "../../services/index.services";
+  gamesService: {
+    findGameById: jest.fn(),
+  },
+
+  tagsService: {
+    findTagById: jest.fn(),
+  },
+}));
+
+import {
+  createNewGameTag,
+  deleteGameTag,
+  findAllGamesTags
+} from "../../controllers/games.tags.controller";
+import {
+  gamesTagsService,
+  gamesService,
+  tagsService
+} from "../../services/index.services";
 import { Request, Response } from "express";
 import {
   mockError,
   mockGameTag1,
   mockGameTagsList
 } from "../mocks/game.tag.mock";
+import { mockGame, mockResultSetHeader } from "../mocks/games.mock";
+import { mockTag1 } from "../mocks/tag.mock";
 
 describe('TESTES DO CONTROLLER GAMES TAGS', () => {
   beforeEach(() => {
@@ -223,6 +241,147 @@ describe('TESTES DO CONTROLLER GAMES TAGS', () => {
             + mockError.message,
         });
       });
+    });
+  });
+
+  describe('DELETAR ASSOCIAÇÃO DE JOGO E CATEGORIA', () => {
+    describe('Caso não consiga deletar a associação', () => {
+      const mockRequest: Partial<Request> = {
+        params: {
+          gameId: '1',
+          tagId: '1',
+        },
+      };
+
+      const mockResponse: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      beforeEach(() => {
+        jest.clearAllMocks();
+      });
+      
+      it('Deve retornar status 404 se o jogo não for encontrado', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue('Não conseguimos encontrar o jogo pelo id 1');
+
+        (tagsService.findTagById as jest.Mock)
+          .mockResolvedValue(mockTag1);
+          
+        await deleteGameTag(
+          mockRequest as Request,
+          mockResponse as Response
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+
+      it('Deve retornar status 404 quando a tag não existe', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue(mockGame);
+    
+        (tagsService.findTagById as jest.Mock)
+          .mockResolvedValue('Não conseguimos encontrar a categoria pelo id 1');
+
+        await deleteGameTag(
+          mockRequest as Request,
+          mockResponse as Response
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+      });
+      
+      it('Deve retornar status 400 se o serviço retorna string ', async () => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue(mockGame);
+
+        (tagsService.findTagById as jest.Mock)
+          .mockResolvedValue(mockTag1);
+
+        (gamesTagsService.deleteGameTag as jest.Mock)
+          .mockResolvedValue(
+            'Não foi possível deletar a associação da ' +
+            'categoria ao jogo com os seguintes dados:\n' +
+            ' gameId: 1\n' +
+            ' tagId: 1\n'
+          );
+
+        await deleteGameTag(
+          mockRequest as Request,
+          mockResponse as Response
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(400);
+        
+        expect(mockResponse.json).toHaveBeenCalledWith({
+            message:
+              'Não foi possível deletar a associação da ' +
+            'categoria ao jogo com os seguintes dados:\n' +
+            ' gameId: 1\n' +
+            ' tagId: 1\n'
+          });
+      });
+
+    describe('Caso consiga deletar a associação', () => {
+      const mockRequest: Partial<Request> = {
+        params: {
+          gameId: '1',
+          tagId: '1',
+        },
+      };
+
+      const mockResponse: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      beforeEach(() => {
+        (gamesService.findGameById as jest.Mock)
+          .mockResolvedValue(mockGame);
+
+        (tagsService.findTagById as jest.Mock)
+          .mockResolvedValue(mockTag1);
+      });
+      
+      it('Deve retornar status 202', async () => {
+        (gamesTagsService.deleteGameTag as jest.Mock)
+          .mockResolvedValue({
+            deletedGameTag: mockGameTag1,
+            message: 'Relacionamento entre ' +
+            mockGame.title.toUpperCase() +
+            ' e ' + mockTag1.title.toUpperCase() + ' excluído com sucesso.',
+          });
+                
+        await deleteGameTag(
+          mockRequest as Request,
+          mockResponse as Response
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(202);
+      });
+
+      it('Deve retornar a confirmação de exclusão', async () => {
+        const expectedResponse = {
+          deletedGameTag: {gameId: 1,
+          tagId: 1,
+          message: 'Relacionamento entre ' +
+            mockGame.title.toUpperCase() +
+            ' e ' + mockTag1.title.toUpperCase() + ' excluído com sucesso.',}
+        };
+        
+        (gamesTagsService.deleteGameTag as jest.Mock)
+          .mockResolvedValue(expectedResponse);
+
+        await deleteGameTag(
+          mockRequest as Request,
+          mockResponse as Response
+        );
+
+        expect(mockResponse.json).toHaveBeenCalledWith(expectedResponse);
+      });
+    });
+
     });
   });
 });
